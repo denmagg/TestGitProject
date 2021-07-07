05c3ee6fc68cbeaf861a3cff57b955b819347b4d
Denis <denmagg.work@gmail.com>
2021-07-07 14:14:45 +0300

    Initial Commit

---
+++ TestGitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitProject
+//
+//  Created by Denis Medvedev on 07/07/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
